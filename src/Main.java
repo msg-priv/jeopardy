367ad84470d78221e67bb01c4d7ba48bf0780e23
msg-priv <142443625+msg-priv@users.noreply.github.com>
2023-08-30 16:40:32 +0000

    for later !!!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import javax.swing.JFrame;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("hey");
+
+        JFrame window = new JFrame();
+        window.setSize(1280, 720);
     }
 }
